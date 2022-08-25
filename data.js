f06776217da996aada48003f8e03c67f819667de
Ben Miller <ben@benrmiller.co.uk>
2022-08-25 16:37:58 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+	scenes: [
+		{
+			id: '0-untitled',
+			name: 'untitled',
+			levels: [
+				{
+					tileSize: 256,
+					size: 256,
+					fallbackOnly: true,
+				},
+				{
+					tileSize: 512,
+					size: 512,
+				},
+				{
+					tileSize: 512,
+					size: 1024,
+				},
+			],
+			faceSize: 1000,
+			initialViewParameters: {
+				pitch: 0,
+				yaw: 0,
+				fov: 1.5707963267948966,
+			},
+			linkHotspots: [],
+			infoHotspots: [
+				{
+					yaw: 0.7151679640352615,
+					pitch: -0.03487364816351679,
+					title: '1',
+					text: 'Text',
+				},
+				{
+					yaw: 1.26317525086988,
+					pitch: -0.053803629898755645,
+					title: '2',
+					text: 'Text',
+				},
+				{
+					yaw: 1.8047752065686762,
+					pitch: -0.04952954193858261,
+					title: '3',
+					text: 'Text',
+				},
+				{
+					yaw: 2.4759064395503074,
+					pitch: -0.055076587440517244,
+					title: '4',
+					text: 'Text',
+				},
+				{
+					yaw: -2.5997878553564675,
+					pitch: -0.0518401996502309,
+					title: '5',
+					text: 'Text',
+				},
+				{
+					yaw: -1.9356881203301608,
+					pitch: -0.04871723800899197,
+					title: '6',
+					text: 'Text',
+				},
+				{
+					yaw: -1.2355709503298549,
+					pitch: -0.05601215891376654,
+					title: '7',
+					text: 'Text',
+				},
+				{
+					yaw: -0.7227086033374661,
+					pitch: -0.04064225268345467,
+					title: '8',
+					text: 'Text',
+				},
+			],
+		},
+	],
+	name: 'Project Title',
+	settings: {
+		mouseViewMode: 'drag',
+		autorotateEnabled: true,
+		fullscreenButton: false,
+		viewControlButtons: false,
+	},
+};
